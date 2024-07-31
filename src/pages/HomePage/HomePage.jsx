8ec69b638c7f125467cee0bbe77cc220e0e9b700
OlhaMasl <maslakok@gmail.com>
2024-07-31 11:05:09 +0300

    trending movies fetch

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import s from "./HomePage.module.css";
+import { fetchMovies } from "../../services/api";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
+
+    const [popularMovies, setpopularMovies] = useState([]);
+     useEffect(() => {
+    const getpopularMovies = async () => {
+      try {
+          const res = await fetchMovies();
+          console.log(res);
+        setpopularMovies(res);
+      } catch (error) {
+          console.log(error);
+      };
+    };
+    getpopularMovies();
+  }, []);
     return (
-        <div>
-            <h1>Home</h1>
+         <div>
+            <ul className={s.popularMoviesList} >
+                {popularMovies.map((movie) => (
+                <li key={movie.id} >
+                        <Link to={movie.id.toString()}>{movie.title}</Link>
+                </li>
+            ))}
+            </ul>
         </div>
     );
 };
