89959896ce7bfa9c7711a5644ea64f6535dea54f
OlhaMasl <maslakok@gmail.com>
2024-08-01 23:37:27 +0300

    add markup for MovieDetailsPage

--- src/pages/HomePage/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/HomePage/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,11 +1,41 @@
 import { useParams } from "react-router-dom";
 import s from "./MovieDetailsPage.module.css";
+import { useEffect, useState } from "react";
+import { fetchMovieById } from "../../../services/api";
 
 const MovieDetailsPage = () => {
     const params = useParams();
+    const [oneMovie, setOneMovie] = useState({});
+    useEffect(() => {
+        const getOneMovie = async () => {
+            try {
+                const response = await fetchMovieById(params.movieId);
+                console.log(response);
+                setOneMovie(response);
+            } catch (error) {
+                console.log(error);
+            }; 
+        };
+        getOneMovie();
+    });
+
+    const getGenres = oneMovie.genres;
+    //   console.log(getGenres);
+    const genres = getGenres.map(genre => genre.name);
+    // console.log(genres);
+
     return (
-        <div>
-            <h1>movie details</h1>
+        <div className={s.movieWrapper}>
+            <img src={`https://image.tmdb.org/t/p/w500${oneMovie.poster_path}` } alt="poster" />
+            <div>
+                <h2>{oneMovie.original_title}</h2>
+                <p>{`Release date: ${oneMovie.release_date}`}</p>
+                <p>{`User Score: ${oneMovie.vote_average}`}</p>
+                <h3>Overview</h3>
+                <p>{oneMovie.overview}</p>
+                <h3>Genres</h3>
+                <p>{genres.join(", ") }</p>
+            </div>
         </div>
     );
 };
